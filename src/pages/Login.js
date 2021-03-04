8cb2499d496009763ba5dc27e89bd2ba5556d8f2
Tony Silva <srcoupe93@outlook.com>
2021-03-03 21:05:26 -0700

    final pages

---
+++ src/pages/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import { InputText } from 'primereact/inputtext';
+import { Card } from 'primereact/card';
+import { Checkbox } from 'primereact/checkbox';
+
+function Login() {
+	return (
+		<div>
+			<h1>Login</h1>
+			<i className='pi pi-sign-in' style={{ fontSize: '3em' }}></i>
+			<Card>
+				<div className='App'>
+					<div className='p-field p-grid'>
+						<label className='p-col-fixed' style={{ width: '100px' }}>
+							Username
+						</label>
+						<div className='p-col'>
+							<InputText id='username' type='text' />
+						</div>
+					</div>
+					<div className='p-field p-grid'>
+						<label className='p-col-fixed' style={{ width: '100px' }}>
+							Password
+						</label>
+						<div className='p-col'>
+							<InputText id='password' type='text' />
+						</div>
+					</div>
+				</div>
+				<div className='p-col-12'>
+					<Checkbox inputId='cb1' value='checkbox'></Checkbox>
+					<label htmlFor='cb1' className='p-checkbox-label'>
+						Remember Me
+					</label>
+				</div>
+			</Card>
+		</div>
+	);
+}
+
+export default Login;
---
+++ src/pages/Register.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import { InputText } from 'primereact/inputtext';
+import { Card } from 'primereact/card';
+
+function Register() {
+	return (
+		<div className='Register'>
+			<h1>Register</h1>
+			<i className='pi pi-key' style={{ fontSize: '4em' }}></i>
+			<Card>
+				<div className='App'>
+					<div className='p-field p-grid'>
+						<label className='p-col-' style={{ width: '100px' }}>
+							Full Name
+						</label>
+						<div className='p-col'>
+							<InputText id='fullname' type='text' />
+						</div>
+					</div>
+					<div className='p-field p-grid'>
+						<label className='p-col-' style={{ width: '100px' }}>
+							Email Address
+						</label>
+						<div className='p-col'>
+							<InputText id='email' type='email' />
+						</div>
+					</div>
+					<div className='p-field p-grid'>
+						<label className='p-col-' style={{ width: '100px' }}>
+							Password
+						</label>
+						<div className='p-col'>
+							<InputText id='password' type='text' />
+						</div>
+					</div>
+					<div className='p-field p-grid'>
+						<label className='p-col-' style={{ width: '100px' }}>
+							Confirm Password
+						</label>
+						<div className='p-col'>
+							<InputText id='confirm' type='text' />
+						</div>
+					</div>
+				</div>
+			</Card>
+		</div>
+	);
+}
+
+export default Register;
